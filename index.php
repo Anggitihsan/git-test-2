3f8fe2076eae31a2de84152ca2b2c3898923d3df
Anggit Ihsananto <ihsananto.anggit@gmail.com>
2022-12-04 08:34:02 +0700

    menambahkan paragraf lorem

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis temporibus magnam mollitia non excepturi fuga, voluptate quia a inventore sequi adipisci delectus, culpa sed, quaerat harum maxime pariatur alias rerum?</p>
 </body>
 </html>
